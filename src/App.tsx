c0c781fec02b2e53a59c500efa513d5b257c5b88
AL <amandarae19@gmail.com>
2026-06-11 14:11:39 -0400

    removing mode 2 functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import LandingPage from "./pages/LandingPage";
 import GameCanvas from "./game/GameCanvas";
-import MissionSelectPage from "./pages/MissionSelectPage";
-import MissionPage from "./pages/MissionPage";
-import ResultsPage from "./pages/ResultsPage";
 
 export default function App() {
   return (
@@ -12,9 +9,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/game" element={<GameCanvas />} />
-        <Route path="/missions" element={<MissionSelectPage />} />
-        <Route path="/mission/:id" element={<MissionPage />} />
-        <Route path="/results" element={<ResultsPage />} />
       </Routes>
       <Analytics />
     </BrowserRouter>
